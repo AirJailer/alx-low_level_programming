42354d2956fd53df7bdaba6a85297773c6966d14
AirJailer <airjailer1@gmail.com>
2023-08-15 16:15:02 -0700

    a

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two intgers
+ *
+ * @n: takes intger as input
+ * @m: takes intger as input
+ *
+ * Return: result of adding
+*/
+
+int add(int n, int m)
+{
+	int result, n, m;
+
+	result = n + m;
+	return (result);
+}
