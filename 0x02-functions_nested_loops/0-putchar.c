3e8f02bfabda1ab830df44343c75b73f82d46782
AirJailer <airjailer1@gmail.com>
2023-08-15 09:55:16 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: prints word _putchar
+ *
+ * Return: (0) success
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
