9de139faddbb7582abdc5bdf34fc0ecd500b2994
AirJailer <airjailer1@gmail.com>
2023-08-17 07:33:46 -0700

    a

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * -mul - multiplies two intgers
+ *
+ *  @a, @b: takes input intgers
+ *
+ *  Return: multiply result
+*/
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
