93d8610983c2619b3b325cafd6d4229b9e617f4b
AirJailer <airjailer1@gmail.com>
2023-08-17 19:59:04 -0700

    a

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
 /**
  * Print_most_numbers - print 0 - 9 apart
  * from 2 and 4 and you can only use _putchar twice
- * Return: 0 in success
+ * Return: Always 0  (success)
 */
 
 void print_most_numbers(void)
