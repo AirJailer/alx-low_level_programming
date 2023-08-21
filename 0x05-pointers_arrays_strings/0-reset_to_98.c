ba32e64ca4ba7ef3fad20a013d547804f87fd282
AirJailer <airjailer1@gmail.com>
2023-08-21 16:29:09 -0700

    a

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as paramter
+ *		and updates the value it points to to 98
+ *
+ * @n: int paramter
+ *
+ * Return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
