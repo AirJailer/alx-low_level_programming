ec8d58872db08b6feb2f5c68047364a2c81116d4
AirJailer <airjailer1@gmail.com>
2023-08-30 13:22:13 -0700

    a

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,6 +3,7 @@
 /**
  * _sqrt_recursion - returns the natural square root of a number.
  * @n: given integer
+ * @val: value to try with
  * Return: natural square root of n
 */
 
@@ -12,11 +13,18 @@ int _sqrt_recursion(int n)
 	return (square(n, 1));
 }
 
+/**
+ * square - gets square of an int
+ * @n: given int
+ * @val: value to try with
+ * Return: int
+*/
+
 int square(int n, int val)
 {
 	if (val * val == n)
 		return (val);
-	else if (cal * val < n)
+	else if (val * val < n)
 		return (square(n, val + 1));
 	else
 		return (-1);
