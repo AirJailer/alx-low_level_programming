ea81af8355dcdfa3dec419a07ef1570e00f6cf46
AirJailer <airjailer1@gmail.com>
2023-09-11 16:50:26 -0700

    m

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,10 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
