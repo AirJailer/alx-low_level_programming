c86aeef242f803f38a55be1acae74d8ba0a87e65
“AirJailer” <airjailer1@gmail.com>
2023-09-21 19:36:09 -0700

    m

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_node - add a node to the start iof the list
+ * @head: address of pointer to head node
+ * @str: str field of the node
+ * Return: size of the list
+ */
+
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_head = malloc(sizeof(list_t));
+
+	if (!head || !new_head)
+		return (NULL);
+	if (str)
+	{
+		new_head->str = strdup(str);
+		if (!new_head->str)
+		{
+			free(new_head);
+			return (NULL);
+		}
+		new_head->len = _strlen(new_head->str);
+	}
+	new_head->next = *head;
+	*head = new_head;
+	return (new_head);
+}
