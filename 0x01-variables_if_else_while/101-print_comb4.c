6f8ee5fa878a17616e18eac7ec2cac6ffe087329
“AirJailer” <airjailer1@gmail.com>
2023-09-24 11:15:28 -0700

    f

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int i, j, m;
+
+    for (i = 48; i <= 57; i++)
+    {
+        for (j = i + 1; j <= 57; j++)
+        {
+            for (m = j + 1; m <= 57; m++)
+            {    
+                if (j == i || m == i || j == m)
+                    continue;
+                else
+                { 
+                    putchar(i);
+                    putchar(j);
+                    putchar(m);
+                }
+                if (i + j != 113)
+                {    putchar(',');
+                    putchar(' ');
+                    
+                }
+                else
+                    putchar('\n');
+                
+            }  
+        }
+    }
+    
+	return (0);
+}
