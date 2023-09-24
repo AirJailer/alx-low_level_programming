fecb6bcee8dd0dd67e133b8c59725c21e6527621
“AirJailer” <airjailer1@gmail.com>
2023-09-24 16:52:51 -0700

    d

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function that prints two two digit combinations
+ * Return: int
+ */
+
+int main(void)
+{
+	int i, j, x, y;
+
+	for (x = 48; x <= 57; x++)
+		for (y = 48; y <= 57; y++)
+		{
+			for (i = x; i <= 57; i++)
+			{
+				for (j = y; j <= 57; j++)
+					if ((x + y) == (i + j))
+						continue;
+					else
+					{
+						putchar(x);
+						putchar(y);
+						putchar(' ');
+						putchar(i);
+						putchar(j);
+						if (x == 57 && y == 56)
+						{
+							putchar('\n');
+						}
+						else
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+			}
+		}
+	return (0);
+}
