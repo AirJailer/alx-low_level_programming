d55b0b3f191de14007217c12ded694a985308f3b
“AirJailer” <airjailer1@gmail.com>
2023-09-25 14:09:25 -0700

    m

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * *add_nodeint_end - adds a new node at the end of a listint_t list.
+ * *add_nodeint - adds a new node at the end of a listint_t list.
  * @head: pointer to first node
  * @n: value for new node
  * Return: pointer to new node
  */
 
-listint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
 
