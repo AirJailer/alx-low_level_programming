d3ef7d0a750ea10002de01f6da5b1c5d6f33084e
“AirJailer” <airjailer1@gmail.com>
2023-09-24 17:00:08 -0700

    d

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,34 +8,33 @@
 
 int main(void)
 {
-	int i, j, x, y;
+    int i, j, x, y;
 
-	for (x = 48; x <= 57; x++)
-		for (y = 48; y <= 57; y++)
-		{
-			for (i = x; i <= 57; i++)
-			{
-				for (j = y; j <= 57; j++)
-					if ((x + y) == (i + j))
-						continue;
-					else
-					{
-						putchar(x);
-						putchar(y);
-						putchar(' ');
-						putchar(i);
-						putchar(j);
-						if (x == 57 && y == 56)
-						{
-							putchar('\n');
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-			}
-		}
-	return (0);
+    for (x = 48; x <= 57; x++)
+        for (y = 48; y <= 57; y++)
+            for (i = x; i <= 57; i++)
+            {
+                for (j = y; j <= 57; j++)
+                    if ((x + y) == (i + j))
+                        continue;
+                    else
+                    {
+                        putchar(x);
+                        putchar(y);
+                        putchar(' ');
+                        putchar(i);
+                        putchar(j);
+                        if (x == 57 && y == 56)
+                        {
+                            putchar('\n');
+                        }
+                        else
+                        {
+                            putchar(',');
+                            putchar(' ');
+                        }
+                    }
+            }
+
+    return (0);
 }
