68ed0a986be099275b8e12ca6910d59231f43e05
“AirJailer” <airjailer1@gmail.com>
2023-10-02 09:46:32 -0700

    k

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	bytes = read(fd, &buf[0], letters);
-	bytes = write(STDOUT_FILEND, &buf[0], bytes);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	clode(fd);
 	return (bytes);
 }
