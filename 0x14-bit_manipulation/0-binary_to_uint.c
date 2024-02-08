194d62714c3314de9bce83fe34129a69b0269c14
AirJailer <airjailer1@gmail.com>
2024-02-08 07:02:01 -0800

    s

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,12 +8,12 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int: num=0;
+	unsigned int num = 0;
 
 	if (!b)
 		return (0);
 
-	while(*b)
+	while (*b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
