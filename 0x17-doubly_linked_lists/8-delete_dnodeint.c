37cbc49f800dc59c77e12d495629793aee07aefc
AirJailer <airjailer1@gmail.com>
2024-03-21 13:56:27 -0700

    m

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at index 
+ * delete_dnodeint_at_index - deletes the node at index
  * @head: pointer to head
  * @index: position of deletion
  * Return: 1 on succes, -1 otherwise
