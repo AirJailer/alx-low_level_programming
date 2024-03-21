35558686bfe84f19b921bec4b122d7d7245933b5
AirJailer <airjailer1@gmail.com>
2024-03-21 13:26:12 -0700

    m

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns length if doublty linked list
+ * @h: address of the head
+ * Return: size of list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+        size_t i = 0;
+
+        while (h)
+        {
+                h = h->next;
+                i++;
+        }
+        return (i);
+}
